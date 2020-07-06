4c1a87c016888bbed7ca48fe54ffa2a662994b15
Malin Yamato Lääkkö <malin@malin.jp>
2020-07-06 09:53:25 +0200

    test

--- stuff.go
+++ stuff.go
@@ -1,5 +1,19 @@
 package main
-import "fmt"
+
+import (
+    "log"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello World"))
+}
+
 func main() {
-    fmt.Println("hello world")
+    http.HandleFunc("/", helloHandler)
+
+    if err := http.ListenAndServe(":9000", nil); err != nil {
+        log.Fatalln("ListenAndServer Error", err)
+    }
+
 }
